f170b12a083ec7d2823c8958a810d4c151642a3d
LucidX190 <142464133+LucidX190@users.noreply.github.com>
2026-04-21 19:07:22 -0400

    Add files via upload

--- UniversalClient.js
+++ UniversalClient.js
@@ -101,7 +101,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\Lucid\AppData\Local\Temp\tmpn2l7732q.js
+// include: C:\Users\Lucid\AppData\Local\Temp\tmpjced8ars.js
 Module["expectedDataFileDownloads"] ??= 0;
 
 Module["expectedDataFileDownloads"]++;
@@ -4231,23 +4231,23 @@ Module["expectedDataFileDownloads"]++;
   });
 })();
 
-// end include: C:\Users\Lucid\AppData\Local\Temp\tmpn2l7732q.js
-// include: C:\Users\Lucid\AppData\Local\Temp\tmptwn7xkcw.js
+// end include: C:\Users\Lucid\AppData\Local\Temp\tmpjced8ars.js
+// include: C:\Users\Lucid\AppData\Local\Temp\tmp9rvts_ud.js
 // All the pre-js content up to here must remain later on, we need to run
 // it.
 if (Module["$ww"] || (typeof ENVIRONMENT_IS_PTHREAD != "undefined" && ENVIRONMENT_IS_PTHREAD)) Module["preRun"] = [];
 
 var necessaryPreJSTasks = Module["preRun"].slice();
 
-// end include: C:\Users\Lucid\AppData\Local\Temp\tmptwn7xkcw.js
-// include: C:\Users\Lucid\AppData\Local\Temp\tmpd22p90ai.js
+// end include: C:\Users\Lucid\AppData\Local\Temp\tmp9rvts_ud.js
+// include: C:\Users\Lucid\AppData\Local\Temp\tmpt_ur9e26.js
 if (!Module["preRun"]) throw "Module.preRun should exist because file support used it; did a pre-js delete it?";
 
 necessaryPreJSTasks.forEach(task => {
   if (Module["preRun"].indexOf(task) < 0) throw "All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?";
 });
 
-// end include: C:\Users\Lucid\AppData\Local\Temp\tmpd22p90ai.js
+// end include: C:\Users\Lucid\AppData\Local\Temp\tmpt_ur9e26.js
 // Sometimes an existing Module object exists with properties
 // meant to overwrite the default module functionality. Here
 // we collect those properties and reapply _after_ we configure
